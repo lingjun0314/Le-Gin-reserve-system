2fa75c398fd38e6db8942ce063be5012c5542477
lingjun0314 <kollug1548@gmail.com>
2024-09-11 23:28:48 +0800

    初始化flutter，新增docker

--- go/main.go
+++ go/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"LeGinReserve/routers"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	//	Set releaseMode
+	gin.SetMode(gin.ReleaseMode)
+
+	//	Disable console color
+	gin.DisableConsoleColor()
+
+	//	Create log file
+	f, err := os.Create("gin.log")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	//	Set log file write in file and console
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
 	r := gin.Default()
 
 	//	Set trust proxy ip
